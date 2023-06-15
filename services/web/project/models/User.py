cb77e244e742c31fe1e5c84bbeaa5dd4d5cecd3c
Sansanto2000 <santiagoandresponteahon@hotmail.com>
2023-06-15 20:11:12 -0300

    Resolviendo modulo de autenticacion de usuarios

--- services/web/project/models/User.py
+++ services/web/project/models/User.py
@@ -15,7 +15,7 @@ class User(db.Model):
         OTHER = 'OTHER'
     
     id:str = db.Column(db.Integer, primary_key=True)
-    username:str = db.Column(db.String(100), nullable=False)
+    username:str = db.Column(db.String(100), nullable=False, unique=True)
     password:str = db.Column(db.String(100), nullable=False)
     role:Role = db.Column(db.Enum(Role), nullable=False)
     gender:Gender = db.Column(db.Enum(Gender), nullable=True)
@@ -23,8 +23,8 @@ class User(db.Model):
     height:float = db.Column(db.Float, nullable=True)
     age:int = db.Column(db.Integer, nullable=True)
 
-    def __init__(self, username:str, password:str, role:Role, gender:Gender=None, weight:float=None, 
-                 height:float=None, age:int=None):
+    def __init__(self, username:str, password:str, role:Role, gender:Gender=None, 
+                 weight:float=None, height:float=None, age:int=None):
         self.username = username
         self.password = password
         self.role = role
@@ -32,3 +32,16 @@ class User(db.Model):
         self.weight = weight
         self.height = height
         self.age = age
+        
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'username': self.username,
+            'password': self.password,
+            'role': self.role.value,
+            'gender': self.gender.value if self.gender else None,
+            'weight': self.weight,
+            'height': self.height,
+            'age': self.age
+        }
+        
--- services/web/project/routes/auth.py
+++ services/web/project/routes/auth.py
@@ -1,11 +1,87 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
+
+from project import db
+from project.models.User import User
 
 auth_api = Blueprint('auth', __name__)
 
 @auth_api.route('/login')
 def login():
-    return 'Página de inicio de sesión'
+    data = request.get_json()
+    if not data:
+        return jsonify(error='Missing JSON data'), 400
+    # Recepcion de campos obligatorios
+    username = data.get('username')
+    password = data.get('password')
+    if not username or not password:
+        return jsonify(error='Missing username or password'), 400
+    
+    # Comprobacion existencia del usuario en la DB
+    user = User.query.filter_by(username=username, password=password).first()
+    if not user:
+        return jsonify(error='Invalid username or password'), 401
+    
+    return jsonify({'message':'Login successful',
+                    'id': user.id}), 200
 
-@auth_api.route('/register')
+@auth_api.route('/register', methods=['POST'])
 def register():
-    return 'Página de registro'
+    data = request.get_json()
+    if not data:
+        return jsonify(error='Missing JSON data'), 400
+    
+    # Recepcion de campos obligatorios
+    username = data.get('username')
+    password = data.get('password')
+    role_str = data.get('role')
+    if not username or not password or not role_str:
+        return jsonify(error='Missing username, password, or role'), 400
+    try:
+        role = User.Role(role_str)
+    except ValueError:
+        valid_roles = [g.value for g in User.Role]
+        return jsonify(error=f'Invalid role. Valid values are: {valid_roles}'), 400
+    
+    # Recepcion de campos opcionales
+    gender_str = data.get('gender')
+    weight_str = data.get('weight')
+    height_str = data.get('height')
+    age_str = data.get('age')
+    
+    gender = None
+    if gender_str:
+        try:
+            gender = User.Gender(gender_str)
+        except ValueError:
+            valid_genders = [g.value for g in User.Gender]
+            return jsonify(error=f'Invalid gender. Valid values are: {valid_genders}'), 400
+        
+    weight = None
+    if weight_str:
+        if not isinstance(weight_str, float):
+            return jsonify(error='Invalid weight. Weight must be a float value.'), 400
+        weight = float(weight_str)
+
+    height = None
+    if height_str:
+        if not isinstance(height_str, float):
+            return jsonify(error='Invalid height. Height must be a float value.'), 400
+        height = float(height_str)
+
+    age = None
+    if age_str:
+        if not isinstance(age_str, int):
+            return jsonify(error='Invalid age. Age must be an integer value.'), 400
+        age = int(age_str)
+    
+    # Comprobacion de valides de los campos en la DB
+    if User.query.filter_by(username=username).first():
+        return jsonify(error='The username is already taken'), 409
+    
+    # Generacion y agregado del usuario en la DB
+    user=User(username=username, password=password, role=role,
+                        gender=gender, weight=weight, height=height, age=age)
+    db.session.add(user)
+    db.session.commit()
+    
+    return jsonify(user.to_dict()), 201
