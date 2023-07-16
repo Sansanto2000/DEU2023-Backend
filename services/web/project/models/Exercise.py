13779d3a12c43f9ef93eed1a455c849d85a51510
Sansanto2000 <santiagoandresponteahon@hotmail.com>
2023-07-16 15:05:06 -0300

    :person_lifting_weights: Agregado de la tabla exercises

---
+++ services/web/project/models/Exercise.py
@@ -0,0 +1,32 @@
+from project import db
+
+class Exercise(db.Model):
+    __tablename__ = "exercises"
+
+    id: str = db.Column(db.Integer, primary_key=True)
+    name: str = db.Column(db.String(128), nullable=False)
+    description: str = db.Column(db.String(512), nullable=True)
+    speed: float = db.Column(db.Float, nullable=True)
+    heart_rate: float = db.Column(db.Float, nullable=True)
+    duration: float = db.Column(db.Float, nullable=True)
+    
+    # exercises: Day = db.Column(db.Enum(Day), nullable=False)
+    # schedules = relationship("Schedule", cascade="all, delete-orphan")
+    
+    def __init__(self, name: str, description: str, speed: float, heart_rate: float, duration: float):
+        self.name = name
+        self.description = description
+        self.speed = speed
+        self.heart_rate = heart_rate
+        self.duration = duration
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'speed': self.speed,
+            'heart_rate': self.heart_rate,
+            'duration': self.duration,
+        }
+    
