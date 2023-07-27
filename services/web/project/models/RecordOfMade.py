3214a957079de6522d08c0289421ff5158164b1b
Sansanto2000 <santiagoandresponteahon@hotmail.com>
2023-07-27 18:44:46 -0300

    :sparkless: Entidad RecordOfMade para realacionar usuarios con Schedules que realizo

---
+++ services/web/project/models/RecordOfMade.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+
+from project import db
+# from project.models.User import User
+# from project.models.Schedule import Schedule
+
+class RecordOfMade(db.Model):
+    __tablename__ = "recordsofmade"
+    
+    id: str = db.Column(db.Integer, primary_key=True)
+
+    user_id: str = db.Column('user_id', db.Integer, db.ForeignKey('users.id'))
+    user = db.relationship('User', back_populates='schedules_realized_list')
+    
+    schedule_id: str = db.Column('schedule_id', db.Integer, db.ForeignKey('schedules.id'))
+    schedule = db.relationship('Schedule', back_populates='realized_list')
+    
+    realized_at: datetime = db.Column('realized_at', db.DateTime, default=lambda: datetime.now())
+    
+    
+    def __init__(self, user, schedule, realized_at: datetime = datetime.now()):
+        self.user = user
+        self.schedule = schedule
+        self.realized_at = realized_at
+
+    def to_dict(self):
+        return {
+            'user_id': self.user_id,
+            'schedule_id': self.schedule_id,
+            'realized_at': self.realized_at,
+        }
